c581fb987f22d2571a046ff01e651ce3862c43dd
Mrityujay Kumar <kumarmrityunjay7034@gmail.com>
2023-05-12 21:55:31 +0530

    pagination added for getting qna training

--- src/App.js
+++ src/App.js
@@ -142,13 +142,15 @@ const App = () => {
                 email
             });
 
-            let botAudio = new Audio(`${process.env.REACT_APP_SERVER_URL}/airlines_new_airlines_greeting_msg_tts.mp3`);
-            botAudio.play();
+            // let botAudio = new Audio(`${process.env.REACT_APP_SERVER_URL}/airlines_new_airlines_greeting_msg_tts.mp3`);
+            // botAudio.play();
 
-            botAudio.onended = () => {
-                audioContextRef.current.resume();
-                setImgSrc(true);
-            }
+            // botAudio.onended = () => {
+            //     audioContextRef.current.resume();
+            //     setImgSrc(true);
+            // }
+
+            playBotAudio(`${process.env.REACT_APP_SERVER_URL}/airlines_new_airlines_greeting_msg_tts.mp3`);
 
         } catch (err) {
             console.log("Error: ", err);
